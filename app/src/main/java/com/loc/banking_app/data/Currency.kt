d86fc60b64309d0760690882a41693788982e148
Mertisler <112168951+Mertisler@users.noreply.github.com>
2024-07-29 12:27:52 +0300

    başarıyla gerçekleştirildi

---
+++ app/src/main/java/com/loc/banking_app/data/Currency.kt
@@ -0,0 +1,10 @@
+package com.loc.banking_app.data
+
+import androidx.compose.ui.graphics.vector.ImageVector
+
+data class Currency (
+    val name: String,
+    val buy: Float,
+    val sell: Float,
+    val icon: ImageVector
+)
